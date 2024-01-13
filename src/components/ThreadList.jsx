08a6d2ed38c6ff758c0a331814b07645fc84b958
Alifiasraa <ajeng1109@gmail.com>
2024-01-14 01:55:58 +0700

    npm-check-updates

--- src/components/ThreadList.jsx
+++ src/components/ThreadList.jsx
@@ -1,36 +1,38 @@
+/* eslint-disable quotes */
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
-import PropTypes from 'prop-types';
-import ThreadCard from './ThreadCard';
+import React from "react";
+import PropTypes from "prop-types";
+import ThreadCard from "./ThreadCard";
 
 function ThreadList({ threads, upvote, downvote }) {
   return (
     <div>
-      {
-        threads.map((thread) => (
-          <ThreadCard
-            {...thread}
-            key={thread.id}
-            upvote={upvote}
-            downvote={downvote}
-          />
-        ))
-       }
+      {threads.map((thread) => (
+        <ThreadCard
+          {...thread}
+          key={thread.id}
+          upvote={upvote}
+          downvote={downvote}
+        />
+      ))}
     </div>
   );
 }
 
 ThreadList.propTypes = {
-  threads: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    body: PropTypes.string.isRequired,
-    category: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired,
-    upVotesBy: PropTypes.arrayOf(PropTypes.string).isRequired,
-    downVotesBy: PropTypes.arrayOf(PropTypes.string).isRequired,
-    totalComments: PropTypes.number.isRequired,
-  }).isRequired,
+  threads: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      body: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+      upVotesBy: PropTypes.arrayOf(PropTypes.string).isRequired,
+      downVotesBy: PropTypes.arrayOf(PropTypes.string).isRequired,
+      totalComments: PropTypes.number.isRequired,
+      // eslint-disable-next-line comma-dangle
+    })
+  ).isRequired,
   upvote: PropTypes.func.isRequired,
   downvote: PropTypes.func.isRequired,
 };
